3384a1ca760c4cae2937bf15e494adf1d601105c
Joseph Gitau <joseph.gitau@canter.fi>
2023-04-28 01:26:23 +0300

    Define sql table for Users

--- sql/v1.sql
+++ sql/v1.sql
@@ -1,14 +1,17 @@
 CREATE TABLE users (
-    "id"         BIGSERIAL   NOT NULL PRIMARY KEY,
-    "first_name" VARCHAR     NOT NULL,
-    "last_name"  VARCHAR     NOT NULL,
-    "email"      VARCHAR     NOT NULL UNIQUE,
-    "password"   VARCHAR     NOT NULL,
-    "username"   VARCHAR     NOT NULL UNIQUE,
-    "role"       VARCHAR     NOT NULL,
-    "active"     BOOLEAN     DEFAULT  true,
-    "created"    timestamptz DEFAULT  CURRENT_TIMESTAMP,
-    "modified"   timestamptz
+    "id"               BIGSERIAL   NOT NULL PRIMARY KEY,
+    "first_name"       VARCHAR     NOT NULL,
+    "last_name"        VARCHAR     NOT NULL,
+    "email"            VARCHAR     NOT NULL UNIQUE,
+    "password"         VARCHAR     NOT NULL,
+    "username"         VARCHAR     NOT NULL UNIQUE,
+    "roles"            JSONB       NOT NULL,
+    "active"           BOOLEAN     DEFAULT  true,
+    "last_login"       timestamptz,
+    "activation_token" UUID,
+    "reset_token"      UUID,
+    "created"          timestamptz DEFAULT  CURRENT_TIMESTAMP,
+    "modified"         timestamptz
 );
 
 CREATE TABLE roles (
