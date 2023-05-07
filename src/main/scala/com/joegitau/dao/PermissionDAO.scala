9c1425a4d9b12d59e6913e1fdc603d57d0f0f186
Joseph Gitau <joseph.gitau@canter.fi>
2023-05-07 14:59:46 +0300

    Implement PermissionDAO retaded fns

--- src/main/scala/com/joegitau/dao/PermissionDAO.scala
+++ src/main/scala/com/joegitau/dao/PermissionDAO.scala
@@ -4,16 +4,17 @@ package dao
 import com.joegitau.models.Permission
 import com.joegitau.slick.CustomPostgresProfile.api._
 import com.joegitau.slick.tables.PermissionTable.Permissions
-import com.joegitau.slick.tables.posLongColumnType
+import com.joegitau.slick.tables._
 import eu.timepit.refined.types.numeric.PosLong
 
+import java.time.Instant
 import scala.concurrent.{ExecutionContext, Future}
 
 trait PermissionDAO[F[_]] {
   def insert(permission: Permission): F[Permission]
   def getById(id: PosLong): F[Option[Permission]]
   def getAll: F[List[Permission]]
-  def update(id: PosLong, newPermission: Permission): F[Option[Permission]]
+  def update(id: PosLong, newPermission: Permission): F[String]
   def delete(id: PosLong): F[String]
 }
 
@@ -31,18 +32,22 @@ class PermissionDAOImpl(db: Database)(implicit ec: ExecutionContext) extends Per
   override def getAll: Future[List[Permission]] =
     db.run(Permissions.result.map(_.toList))
 
+  override def update(id: PosLong, newPermission: Permission): Future[String] = {
+    val query = for {
+      permission <- Permissions if permission.id === id
+    } yield (permission.name, permission.modified)
+
+    val action = query.update((newPermission.name, Some(Instant.now)))
+
+    db.run(action.transactionally)
+      .map(_ => s"Successfully updated permission with id: $id")
+      .recover { case ex: Exception => ex.getMessage }
+  }
+
   override def delete(id: PosLong): Future[String] = {
     db.run(queryById(id).delete)
       .map(_ => s"Successfully deleted permission with id: $id")
+      .recover { case ex: Exception => ex.getMessage }
   }
 
-  override def update(id: PosLong, newPermission: Permission): Future[Option[Permission]] = {
-    // query by id
-    // if found, we update it, if not return an option
-    val permToUpdate = queryById(id).result.headOption
-    permToUpdate match {
-      case perm => ???
-
-    }
-  }
 }
